4a9fced12a3fb2adf832d3f8404abfd63da65ab2
Sarai Rojas <princesnicole051@hotmail.com>
2022-05-26 15:01:05 +0000

    LEER TECLADO

--- EjerciciosClase2/leerteclado.java
+++ EjerciciosClase2/leerteclado.java
@@ -1,7 +1,13 @@
 package EjerciciosClase2;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 public class leerteclado {
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); //Ya tenemos
     public int  print(String mensaje, int valor) {
+        System.out.println(mensaje+":");
+        try {
+            valor=Integer.parseInt(br.readLine());
+        }
         return 0;
     }
     public String print(String mensaje, String valor) {
